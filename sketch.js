bbf823c3db37d961a7c356c0a196a0891b84bce7
jjuarez19 <jjuar2@uic.edu>
2021-09-15 00:54:08 -0500

    better bouncing ball

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
-var x = 0;
-var speed = 3;
+var ball = {
+  x: 300,
+  y: 200,
+  xspeed:4,
+  yspeed:-3
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,15 +11,27 @@ function setup() {
   
 function draw() {
   background(0);
+  move();
+  bounce();
+  display();
+}
+
+function move() {
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y + ball.yspeed;
+}
+
+function bounce() {
+  if (ball.x > width || ball.x < 0) {
+    ball.xspeed = ball.xspeed * -1;
+  }
+  if (ball.y > height || ball.y < 0) {
+    ball.yspeed = ball.yspeed * -1;
+  }
+}  
+function display(){
   stroke(225);
   strokeWeight(4);
   noFill();
- 
-  ellipse(x, 200, 100, 100);
-  
-  if (x > width || x < 0) {
-    speed = speed * -1;
-  }
-  
-  x = x + speed;
+  ellipse(ball.x, ball.y, 50);
 }
