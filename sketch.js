e3342908333ab85a8f3acc5a37d09864215376f2
jjuarez19 <jjuar2@uic.edu>
2021-09-21 20:20:16 -0500

    added a ball to the sketch

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
+var ball = {
+  x: 300,
+  y: 200,
+  xspeed:4,
+  yspeed:-3
+}
 var bgColor;
 let angle = 0;
 
@@ -12,6 +18,39 @@ function draw() {
   translate(width/2, height/2);
   rotate(angle);
   fill(0);
+  
+  // rectMode(CENTER);
+  // rect(0,0,100,50);
+  
+  move();
+  bounce();
+  display();
+  triforce();
+}  
+
+function move() {
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y + ball.yspeed;
+}
+
+function bounce() {
+  if (ball.x > width || ball.x < 0) {
+    ball.xspeed = ball.xspeed * -1;
+  }
+  if (ball.y > height || ball.y < 0) {
+    ball.yspeed = ball.yspeed * -1;
+  }
+}
+
+function display(){
+  stroke(0);
+  strokeWeight(4);
+  noFill();
+  ellipse(ball.x, ball.y, 50);
+  ellipse(200,200,25);
+}
+
+function triforce(){ 
   triangle(50, 250, 250, 250, 150, 50);
   fill(255, 255, 0);
   triangle(50, 250, 150, 250, 100, 150);
@@ -20,7 +59,7 @@ function draw() {
   pop();
   angle += radians(2);
 }
+
 function mousePressed() {
   bgColor = color( random(255), random(255), random(255) );
-
 }
