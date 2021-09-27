41593d74e697ce50372f354a16ad16187c142a93
jjuarez19 <jjuar2@uic.edu>
2021-09-27 00:42:26 -0500

    added rectangle

--- sketch.js
+++ sketch.js
@@ -7,12 +7,15 @@ var ball = {
 
 var bgColor;
 var ballColor;
+var rectColor;
 let angle = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   bgColor = color( random(255), random(255), random(255) );
   ballColor = color(random(255), random(255), random(255) );
+  rectColor = color(random(255), random(255), random(255) );
+
 }
 
 function draw() {
@@ -24,17 +27,18 @@ function draw() {
   fill(0);
   
   triforce();
+
   move();
   bounce();
   display();
  
+  rectangle();
+  
 }  
-
 function move() {
   ball.x = ball.x + ball.xspeed;
   ball.y = ball.y + ball.yspeed;
 }
-
 function bounce() {
   if (ball.x > width || ball.x < 0) {
     ball.xspeed = ball.xspeed * -1;
@@ -53,8 +57,7 @@ function display(){
 
 }
 
-function triforce(){ 
-  
+function triforce(){  
   triangle(50, 250, 250, 250, 150, 50);
   fill(255, 255, 0);
   triangle(50, 250, 150, 250, 100, 150);
@@ -64,7 +67,16 @@ function triforce(){
   angle += radians(2);
 }
 
+function rectangle(){
+  fill(rectColor);
+  translate(width / 2, height / 2);
+  let a = atan2(mouseY - height / 2, mouseX - width / 2);
+  rotate(a);
+  rect(0, 0, 140, 30);
+}
+
 function mousePressed() {
   bgColor = color( random(255), random(255), random(255) );
   ballColor = color(random(255), random(255), random(255) );
+  rectColor = color(random(255), random(255), random(255) );
 }
