590f6209d34fc5ac0489b9d05b202bed174cef36
jjuarez19 <jjuar2@uic.edu>
2021-09-27 11:03:42 -0500

    .

--- sketch.js
+++ sketch.js
@@ -1,95 +1,29 @@
-var ball = {
-  x: 300,
-  y: 400,
-  xspeed:6,
-  yspeed:-6,
-}
-
-var ballColor;
-let angle = 0;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ballColor = color(random(255), random(255), random(255));
-  rupeeColor = color(random(255), random(255), random(255) );
-  alert("Repeatedly click your mouse to change the color of the background being drawn!")
+  createCanvas(600, 200);
 }
 
 function draw() {
-  push();
-  translate(width/2, height/2);
-  rotate(angle);
+  background(0);
   
-  fill(0);
+  let posX = width/2;
+  let posY = height/2;
   
-  triforce();
+  let angle = Math.atan2(mouseY-posY, mouseX-posX);
 
-  move();
-  bounce();
-  display();
- 
-  rupee();
-  
-} 
+  translate(posX, posY);
+  rotate(angle)
+  // rotate(angle + radians(-90))
 
-function move() {
-  ball.x = ball.x + ball.xspeed;
-  ball.y = ball.y + ball.yspeed;
-}
-
-function bounce() {
-  if (ball.x > width || ball.x < 0) {
-    ball.xspeed = ball.xspeed * -1;
-  }
-  if (ball.y > height || ball.y < 0) {
-    ball.yspeed = ball.yspeed * -1;
-  }
-}
-
-function display(){
-  stroke(0);
-  strokeWeight(1);
-  fill(ballColor);
-  ellipse(ball.x, ball.y, 50);
-  ellipse(ball.x, ball.y, 50);
-
-}
-
-function triforce(){  
-  triangle(50, 250, 250, 250, 150, 50);
-  fill(255, 255, 0);
-  triangle(50, 250, 150, 250, 100, 150);
-  triangle(150, 250, 250, 250, 200, 150);
-  triangle(100, 150, 200, 150, 150, 50);
-  pop();
-  angle += radians(2);
-}
-  
-function rupee() {
-   
-   translate(width / 2, height / 2);
-   stroke(0);
-   strokeWeight(2);
-   fill(rupeeColor);
-   let a = atan2(mouseY - height / 2, mouseX - width / 2);
-   rotate(a);
-   push();
-   beginShape();
-   vertex(0,-60);
-   vertex(30,-20);
-   vertex(30, 30);
-   vertex(0, 70);
-   vertex(-30, 30);
-   vertex(-30,-20);
-   vertex(0,-60);
-   endShape();
-   pop();
-}
-
-function mousePressed() {
-  bgColor = color( random(255), random(255), random(255) );
-  ballColor = color(random(255), random(255), random(255) );
-  rectColor = color(random(255), random(255), random(255) );
-  rupeeColor = color(random(255), random(255), random(255) );
-
+  stroke(255, 255, 0)
+  fill(255, 0, 0)
+  beginShape();
+  vertex(-3, -3);
+  vertex(50, -3);
+  vertex(50, -6);
+  vertex(60, 0);
+  vertex(50, 6);
+  vertex(50, 3);
+  vertex(-3, 3);
+  vertex(-3, -3);
+  endShape()
 }
