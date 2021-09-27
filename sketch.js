d624a0a2a381acac848659d90397dba2281ce2c5
jjuarez19 <jjuar2@uic.edu>
2021-09-27 00:53:49 -0500

    added center rect

--- sketch.js
+++ sketch.js
@@ -1,29 +1,82 @@
+var ball = {
+  x: 300,
+  y: 400,
+  xspeed:6,
+  yspeed:-6,
+}
+
+var bgColor;
+var ballColor;
+var rectColor;
+let angle = 0;
+
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(windowWidth, windowHeight);
+  bgColor = color( random(255), random(255), random(255) );
+  ballColor = color(random(255), random(255), random(255) );
+  rectColor = color(random(255), random(255), random(255) );
+
 }
 
 function draw() {
-  background(0);
+  // background(bgColor);
+  push();
+  translate(width/2, height/2);
+  rotate(angle);
   
-  let posX = width/2;
-  let posY = height/2;
+  fill(0);
   
-  let angle = Math.atan2(mouseY-posY, mouseX-posX);
+  triforce();
 
-  translate(posX, posY);
-  rotate(angle)
-  // rotate(angle + radians(-90))
+  move();
+  bounce();
+  display();
+ 
+  rectangle();
+  
+}  
+function move() {
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y + ball.yspeed;
+}
+function bounce() {
+  if (ball.x > width || ball.x < 0) {
+    ball.xspeed = ball.xspeed * -1;
+  }
+  if (ball.y > height || ball.y < 0) {
+    ball.yspeed = ball.yspeed * -1;
+  }
+}
 
-  stroke(255, 255, 0)
-  fill(255, 0, 0)
-  beginShape();
-  vertex(-3, -3);
-  vertex(50, -3);
-  vertex(50, -6);
-  vertex(60, 0);
-  vertex(50, 6);
-  vertex(50, 3);
-  vertex(-3, 3);
-  vertex(-3, -3);
-  endShape()
+function display(){
+  stroke(0);
+  strokeWeight(4);
+  fill(ballColor);
+  ellipse(ball.x, ball.y, 50);
+  ellipse(ball.x, ball.y, 50);
+
+}
+
+function triforce(){  
+  triangle(50, 250, 250, 250, 150, 50);
+  fill(255, 255, 0);
+  triangle(50, 250, 150, 250, 100, 150);
+  triangle(150, 250, 250, 250, 200, 150);
+  triangle(100, 150, 200, 150, 150, 50);
+  pop();
+  angle += radians(2);
+}
+
+function rectangle(){
+  fill(rectColor);
+  translate(width / 2, height / 2);
+  let a = atan2(mouseY - height / 2, mouseX - width / 2);
+  rotate(a);
+  rect(0, 0, 140, 30);
+}
+
+function mousePressed() {
+  bgColor = color( random(255), random(255), random(255) );
+  ballColor = color(random(255), random(255), random(255) );
+  rectColor = color(random(255), random(255), random(255) );
 }
